0e57b3c862cd8af81b714778ab45fc4375204c16
stix-italian <stix.italian@gmail.com>
2021-05-03 21:47:40 -0600

    add floating nav, character select, character background

--- characters/main.js
+++ characters/main.js
@@ -27,11 +27,31 @@ othersButton.textContent = 'Other Characters'
 mainHeader.appendChild(othersButton)
 othersButton.addEventListener('click', () => populateDOM(otherCharacters))
 
+const planetSelect = document.createElement('select')
+console.log('what the hell')
+planetSelect.id = "planSelect"
+planetSelect.textContent = 'Characters From'
+mainHeader.appendChild(planetSelect)
+planetSelect.addEventListener("change",planetFilter())
+
+
+const discButton = document.createElement('option')
+discButton.textContent = 'Characters From..'
+discButton.value = ''
+planetSelect.appendChild(discButton)
+
+const tatButton = document.createElement('option')
+tatButton.textContent = 'Characters From Tatooine'
+tatButton.value = 'tat'
+planetSelect.appendChild(tatButton)
+// tatButton.addEventListener('click', () => populateDOM(TatCharacters))
+
 
 
 const maleCharacters = people.filter(person => person.gender === 'male')
 const femaleCharacters = people.filter(person => person.gender === 'female')
 const allCharacters = people.filter(person => person.gender !== '')
+const TatCharacters = people.filter(person => person.homeworld === 'https://swapi.co/api/planets/1/')
 const otherCharacters = people.filter(person => {
     if (person.gender === 'n/a' ||
         person.gender === 'hermaphrodite') {
@@ -39,6 +59,13 @@ const otherCharacters = people.filter(person => {
     }
 const allCharacters = people.filter(person => person.gender === 'n/a')
 })
+
+// const TatCharacters = people.filter(person => {//person.homeworld === "https://swapi.co/api/planets/1/")
+//     for(i = 0; i < person.films.length; i++){
+//         if (person.films[i] === 1) return person
+//     }
+//     const allCharacters = people.filter(person => person.gender === 'n/a')
+// })
 
 function populateDOM(characters) {
     removeChildren(mainElement)
@@ -54,6 +81,17 @@ function populateDOM(characters) {
 
         mainElement.appendChild(charFigure)
     })
+}
+
+function planetFilter(){
+    console.log('It calls to me')
+    var val = document.getElementById('planSelect').value
+    console.log('val = '+val)
+    if(val === 'tat'){
+        console.log("It's Working!!")
+        const TatCharacters = people.filter(person => person.homeworld === 'https://swapi.co/api/planets/1/')
+        populateDOM(TatCharacters)
+    }
 }
 
 populateDOM(allCharacters)
